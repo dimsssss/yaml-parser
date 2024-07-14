cb45e84ec939e0d0491525a683b80d196ab04fea
dev <caruen@hanmail.net>
2024-07-14 19:46:37 +0900

    test: parser 테스트 추가

---
+++ tests/parser.spec.ts
@@ -0,0 +1,95 @@
+import fs from 'fs/promises';
+import { Lexer } from '../src/lexer';
+import { Parser } from '../src/parser';
+
+describe('parser test', () => {
+  let objectYaml;
+  let numberYaml;
+  let booleanYaml;
+  let nullYaml;
+  let concatStringYaml;
+  let stringYaml;
+
+  beforeAll(async () => {
+    const [
+      objectBuffer,
+      numberBuffer,
+      booleanBuffer,
+      nullBuffer,
+      concatStringBuffer,
+      stringBuffer,
+    ] = await Promise.all([
+      fs.readFile('tests/file/parser/object-parser.yaml', 'utf8'),
+      fs.readFile('tests/file/parser/number-parser.yaml', 'utf8'),
+      fs.readFile('tests/file/parser/boolean-parser.yaml', 'utf8'),
+      fs.readFile('tests/file/parser/null-parser.yaml', 'utf8'),
+      fs.readFile('tests/file/parser/concat-string-parser.yaml', 'utf8'),
+      fs.readFile('tests/file/parser/string-parser.yaml', 'utf8'),
+    ]);
+
+    objectYaml = objectBuffer.toString();
+    numberYaml = numberBuffer.toString();
+    booleanYaml = booleanBuffer.toString();
+    nullYaml = nullBuffer.toString();
+    concatStringYaml = concatStringBuffer.toString();
+    stringYaml = stringBuffer.toString();
+  });
+
+  test('parser should parse object', async () => {
+    const lexer = new Lexer(objectYaml);
+    const parser = new Parser(lexer.tokenizer());
+    const result = parser.parse();
+
+    expect(JSON.stringify(result)).toBe(
+      `{"name":"YAML Parser","spec":{"name":{"type":"good"}}}`,
+    );
+  });
+
+  test('parser should parse number', async () => {
+    const lexer = new Lexer(numberYaml);
+    const parser = new Parser(lexer.tokenizer());
+    const result = parser.parse();
+
+    expect(result.number).toBe(1);
+    expect(JSON.stringify(result)).toBe(`{"number":1}`);
+  });
+
+  test('parser should parse boolean', async () => {
+    const lexer = new Lexer(booleanYaml);
+    const parser = new Parser(lexer.tokenizer());
+    const result = parser.parse();
+
+    expect(result.isGood).toBeTruthy();
+    expect(result.isNotGood).toBeFalsy();
+    expect(JSON.stringify(result)).toBe(`{"isGood":true,"isNotGood":false}`);
+  });
+
+  test('parser should parse null', async () => {
+    const lexer = new Lexer(nullYaml);
+    const parser = new Parser(lexer.tokenizer());
+    const result = parser.parse();
+
+    expect(result.good).toBeNull();
+    expect(JSON.stringify(result)).toBe(`{"good":null}`);
+  });
+
+  test('parser should parse concat string', async () => {
+    const lexer = new Lexer(concatStringYaml);
+    const parser = new Parser(lexer.tokenizer());
+    const result = parser.parse();
+
+    expect(result.message).toBe('this is an example yaml parser');
+    expect(JSON.stringify(result)).toBe(
+      `{"message":"this is an example yaml parser"}`,
+    );
+  });
+
+  test('parser should parse string', async () => {
+    const lexer = new Lexer(stringYaml);
+    const parser = new Parser(lexer.tokenizer());
+    const result = parser.parse();
+
+    expect(result.string).toBe('it is string');
+    expect(JSON.stringify(result)).toBe(`{"string":"it is string"}`);
+  });
+});
